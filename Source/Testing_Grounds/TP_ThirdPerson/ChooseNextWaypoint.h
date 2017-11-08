1f21a89af3c37b9283ddb4c09c8405112ed0530b
Frédéric Poissant <frederic.r.poissant@gmail.com>
2017-11-08 14:34:09 -0500

    Lecture_220

--- Source/Testing_Grounds/TP_ThirdPerson/ChooseNextWaypoint.h
+++ Source/Testing_Grounds/TP_ThirdPerson/ChooseNextWaypoint.h
@@ -19,4 +19,7 @@ class TESTING_GROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 protected:
 	UPROPERTY(EditAnywhere, Category = "Blackboard")
 	struct FBlackboardKeySelector IndexKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WaypointKey;
 };
