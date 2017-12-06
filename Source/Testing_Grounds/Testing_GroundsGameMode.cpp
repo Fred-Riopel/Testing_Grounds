d35f4414663c6cbcbe1c144687fd29091e418342
Frédéric Poissant <frederic.r.poissant@gmail.com>
2017-12-06 11:02:19 -0500

    Lecture_282 (with AGun bug)

--- Source/Testing_Grounds/Testing_GroundsGameMode.cpp
+++ Source/Testing_Grounds/Testing_GroundsGameMode.cpp
@@ -9,7 +9,7 @@ ATesting_GroundsGameMode::ATesting_GroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behaviour/Character_BP"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behaviour/BP_Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
