80c76f28a2dd502ea79cead573b4f6c9c2ce8a3f
Frédéric Poissant <frederic.r.poissant@gmail.com>
2017-12-21 13:56:10 -0500

    Lecture_320

--- Source/Testing_Grounds/Terrain/GrassComponent.cpp
+++ Source/Testing_Grounds/Terrain/GrassComponent.cpp
@@ -11,8 +11,6 @@ UGrassComponent::UGrassComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -37,6 +35,4 @@ void UGrassComponent::SpawnGrass()
 void UGrassComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
 }
